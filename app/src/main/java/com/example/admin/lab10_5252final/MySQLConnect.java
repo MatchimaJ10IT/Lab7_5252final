a0b904c3266e8a350d9db0a0fa60f12b2954d850
JMatchima <44288043+JMatchima@users.noreply.github.com>
2018-11-26 01:41:36 +0700

    123

---
+++ app/src/main/java/com/example/admin/lab10_5252final/MySQLConnect.java
@@ -0,0 +1,74 @@
+package com.example.admin.lab10_5252final;
+
+import android.app.Activity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MySQLConnect {
+    private final Activity main;
+    private List<String> list;
+    private String URL = "10.199.3.64";
+
+    public MySQLConnect() { this.main = null;}
+
+    public MySQLConnect(Activity m){
+        main = m;
+        list = new ArrayList<String>();
+    }
+
+    public List<String> getData() {
+        String url = URL + "/sqlinsert/getData.php";
+        StringRequest stringRequest = new StringRequest(url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if(response == null){
+                    Toast.makeText(main.getApplicationContext(),"failed",Toast.LENGTH_SHORT).show();
+                }else{
+                    Showjson(response);
+                    Toast.makeText(main.getApplicationContext(),"Success",Toast.LENGTH_SHORT).show();
+                }
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("TAG123",error.toString());
+                    }
+                }
+        );
+        RequestQueue requestQueue = Volley.newRequestQueue(main.getApplicationContext());
+        requestQueue.add(stringRequest);
+        return list;
+    }
+    public void Showjson(String response){
+        String comment = "";
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray result = jsonObject.getJSONArray("result");
+            for(int i = 0;i<result.length();i++){
+                JSONObject collect = result.getJSONObject(i);
+                comment = collect.getString("comment");
+                list.add(comment);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
